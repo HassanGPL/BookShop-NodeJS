efff78fc34bde2db4736dd0da0015423c7f75eda
Hassan Ahmed <ahmedhassanmana@gmail.com>
2026-07-09 22:12:07 +0300

    parsing request body and work with it's data

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.write('<html>')
         res.write('<head><title>Message!</title></head>')
@@ -13,6 +15,20 @@ const server = http.createServer((req, res) => {
         res.write('</body>')
         res.write('</html>')
         return res.end()
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
     }
     res.write('<html>')
     res.write('<head><title>First Page</title></head>')
