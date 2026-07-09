0c6b4a3a639e9857def61db531bbcc2744913143
Hassan Ahmed <ahmedhassanmana@gmail.com>
2026-07-09 21:36:09 +0300

    routing request

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<head><title>Message!</title></head>')
+        res.write('<body>')
+        res.write('<form action="/message" method="POST">')
+        res.write('<input type="text" name="message"></input>')
+        res.write('<button type="submit">Send</button>')
+        res.write('</form>')
+        res.write('</body>')
+        res.write('</html>')
+        return res.end()
+    }
     res.write('<html>')
     res.write('<head><title>First Page</title></head>')
-    res.write('<body><h1>Hello From My NodeJS Server</h1></body>')
+    res.write('<body><h1>Hello From My NodeJS Server!</h1></body>')
     res.write('</html>')
     res.end()
 });
