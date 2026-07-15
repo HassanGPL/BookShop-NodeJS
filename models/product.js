8119cbbc5d7ed14b636c450bc35694b3d69ffe92
Hassan Ahmed <ahmedhassanmana@gmail.com>
2026-07-15 21:04:53 +0300

    Storing and Fitching Products Data with File Storage

--- models/product.js
+++ models/product.js
@@ -1,4 +1,17 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+const util = require('../utils/path');
+
+const p = path.join(util, 'data', 'products.json');
+const getProductsFromFile = (cb) => {
+    fs.readFile(p, (err, data) => {
+        if (err) {
+            return cb([]);
+        }
+        cb(JSON.parse(data));
+    })
+}
+
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +19,17 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p,
+                JSON.stringify(products),
+                (err) => {
+                    console.log(err);
+                });
+        })
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        getProductsFromFile(cb)
     }
 }
