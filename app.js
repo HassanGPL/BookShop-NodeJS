fae95889079ceb6fb4481b49962816bbab90d2ca
Hassan Ahmed <ahmedhassanmana@gmail.com>
2026-07-23 23:51:55 +0300

    Adding a One-To-Many Relationship (USER - PRODUCT)

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const sequelize = require('./utils/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const errorController = require('./controllers/error');
 
@@ -17,13 +19,34 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constrains: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequelize.sync()
     .then(result => {
+        return user = User.findByPk(1);
+    }).then(user => {
+        if (!user) {
+            return User.create({ name: 'Hassan', email: 'hassan@test.com' });
+        }
+        return user
+    }).then(user => {
+        // console.log(user);
         app.listen(3000);
     })
     .catch(err => console.log(err));
