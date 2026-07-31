6c0a3280008117ea1adc4232d88b5a6184df23cd
Hassan Ahmed <ahmedhassanmana@gmail.com>
2026-07-31 23:00:39 +0300

    Storing Cartitems as Orderitems

--- controllers/shop.js
+++ controllers/shop.js
@@ -82,6 +82,29 @@ exports.postCart = (req, res, next) => {
         .catch(err => console.log(err));
 }
 
+exports.postOrder = (req, res, next) => {
+    const user = req.user;
+    user.getCart()
+        .then(cart => {
+            return cart.getProducts();
+        })
+        .then(products => {
+            return user
+                .createOrder()
+                .then(order => {
+                    return order.addProducts(
+                        products.map(product => {
+                            product.orderItem = { quantity: product.cartItem.quantity };
+                            return product;
+                        }))
+                }).catch(err => console.log(err));
+        })
+        .then(result => {
+            res.redirect('/orders');
+        })
+        .catch(err => console.log(err));
+}
+
 exports.getOrders = (req, res, next) => {
     res.render('shop/orders', {
         path: '/orders',
