a5100ce91672a3e864e35dad5a3a10040de4cda3
Hassan Ahmed <ahmedhassanmana@gmail.com>
2026-08-08 08:28:09 +0300

    Connect to MongoDB

--- utils/database.js
+++ utils/database.js
@@ -1,9 +1,29 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
 
-const sequelize = new Sequelize('node-complete', 'root', '1234',
-    {
-        host: 'localhost',
-        dialect: 'mysql'
-    });
+const mongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+let _db;
+
+const mongoConnection = (cb) => {
+    mongoClient
+        .connect('mongodb+srv://hassanahmed11920_db_user:Jtzabjwln4UxfuJF@cluster0.bbisxfp.mongodb.net/shop?appName=Cluster0')
+        .then(client => {
+            console.log('MONGODB CONNECTED!');
+            _db = client.db;
+            cb();
+        })
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
+}
+
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No Database Found!';
+}
+
+exports.mongoConnection = mongoConnection;
+exports.getDb = getDb;
