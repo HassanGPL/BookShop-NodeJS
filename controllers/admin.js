14b84ac980ed7a977b8d001b11319aaac113e6f3
Hassan Ahmed <ahmedhassanmana@gmail.com>
2026-08-17 22:21:21 +0300

    Deleting Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -84,7 +84,7 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const productId = req.body.productId;
-    Product.deleteById(productId)
+    Product.findByIdAndDelete(productId)
         .then(() => {
             console.log('DELETED PRODUCT SUCCESSFULLY!')
             res.redirect('/admin/products');
