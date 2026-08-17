a4656310dc45de4ffdb12f6f3028c7e865e8255d
Hassan Ahmed <ahmedhassanmana@gmail.com>
2026-08-17 22:04:05 +0300

    Fetching All Products

--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const adminController = require('../controllers/admin');
 
-// // /admin/products //GET
-// router.get('/products', adminController.getProducts);
+// /admin/products //GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product //GET
 router.get('/add-product', adminController.getAddProduct);
