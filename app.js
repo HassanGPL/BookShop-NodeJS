1dc5a406bf7cbb57c3015178525e8d22b6b4a2a0
Hassan Ahmed <ahmedhassanmana@gmail.com>
2026-08-17 21:59:09 +0300

    Saving Product Data Through Mongoose

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const shopRoutes = require('./routes/shop');
 
 const errorController = require('./controllers/error');
 
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -20,14 +20,14 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User.findById('6a78fbec20e75765bc03dbc2')
-        .then(user => {
-            req.user = new User(user.name, user.email, user.cart, user._id);
-            next();
-        })
-        .catch(err => console.log(err));
-})
+// app.use((req, res, next) => {
+//     User.findById('6a78fbec20e75765bc03dbc2')
+//         .then(user => {
+//             req.user = new User(user.name, user.email, user.cart, user._id);
+//             next();
+//         })
+//         .catch(err => console.log(err));
+// })
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
--- routes/admin.js
+++ routes/admin.js
@@ -6,11 +6,11 @@ const adminController = require('../controllers/admin');
 // // /admin/products //GET
 // router.get('/products', adminController.getProducts);
 
-// // /admin/add-product //GET
-// router.get('/add-product', adminController.getAddProduct);
+// /admin/add-product //GET
+router.get('/add-product', adminController.getAddProduct);
 
-// // /admin/add-product //POST
-// router.post('/add-product', adminController.postAddProduct);
+// /admin/add-product //POST
+router.post('/add-product', adminController.postAddProduct);
 
 // // /admin/edit-product/productId?edit=true //GET
 // router.get('/edit-product/:productId', adminController.getEditProduct);
